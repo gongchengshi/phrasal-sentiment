17035c35a8eb9004a370c2318e2b006090c76c5b
Nick Chen <nickchen@gmail.com>
2014-05-20 20:43:51 -0700

    do split in mallet

--- run.sh
+++ run.sh
@@ -3,10 +3,5 @@
 export JAVA_HOME=/opt/jdk1.8.0/jre
 mvn package
 
-head -n 172 project_output/data.txt > project_output/train.txt
-tail -n 688 project_output/data.txt > project_output/test.txt
-
-mallet import-file --input project_output/train.txt --output project_output/train.vectors
-mallet import-file --input project_output/test.txt --output project_output/test.vectors --use-pipe-from project_output/train.vectors
-
-vectors2classify --training-file project_output/train.vectors  --testing-file project_output/test.vectors --trainer MaxEnt --output-classifier project_output/me-model      > project_output/me.stdout 2>project_output/me.stderr
+csv2vectors --input project_output/data.txt --output data.vectors
+vectors2classify --input data.vectors --training-portion 0.6 --num-trials 3 --trainer MaxEnt 
