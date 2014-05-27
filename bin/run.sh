70b5e17f221e5156e850056301f74904f8698c13
Nick Chen <nickchen@gmail.com>
2014-05-26 17:14:57 -0700

    generate experiments in run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -5,6 +5,8 @@ mvn package
 
 /opt/jdk1.8.0/jre/bin/java -jar target/phrasal_sentiment-1.0-SNAPSHOT-jar-with-dependencies.jar
 
-bin/experiment_split.py --project_output project_output --random_sampling
+echo "genearting experiment files, call project_output/run_exp.sh to run"
+bin/experiment_split.py --project_output ./project_output/ --features=5 --experiments=60 --generate_run
+
 csv2vectors --input project_output/data_working.txt --output project_output/data.vectors
 vectors2classify --input project_output/data.vectors --training-portion 0.6 --num-trials 3 --trainer MaxEnt > project_output/data_working_maxin
